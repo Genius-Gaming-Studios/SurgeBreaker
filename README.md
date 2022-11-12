617382affaa239760a5eacefac5cafee334b8c18
DA-Nutt <devon.nutting@hgs.hiddengeniusproject.org>
2022-11-11 18:04:46 -0800

    Added GDD

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 To open this project in Unity, you must use Unity __2021.3.11f1__. You can install Unity 2021.3.11 here: https://unity3d.com/unity/whats-new/2021.3.11
 
 While installing Unity 2021.3.11, ensure that you have added the **Windows**, **MacOS** and **Linux** modules to the installation. 
+
+
+## Documentation
+This is the Game Design Documentation (GDD) for Titan Knights: https://docs.google.com/document/d/151hSkVtsA0YYmxjPKl8OPJCGj4EHKUAGi7DemlB3rQM/edit?pli=1# 
--- Titan Knight (HGU Project)/Assets/Scripts/Enemy.cs
+++ Titan Knight (HGU Project)/Assets/Scripts/Enemy.cs
@@ -1,37 +1,134 @@
+/* MARK GASKINS
+ * 
+ * THIS SHOULD BE ATTACHED TO THE PARENT OBJECT OF AN ENEMY AT ALL TIMES
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
-    [Tooltip("The enemy's start health.")] [SerializeField]  [Range(1, 2000)] int EnemyHealth;
+    [Header("Options")]
+    //[SerializeField] int walkSpeed;
+    [Tooltip("The attack damage that this enemy does.")] [SerializeField] int attackDamage = 5;
+    [Tooltip("This is the time between her attacks.")] [SerializeField] [Range(0.0f, 3.0f)] float attackRate = 1;
+    [Tooltip("The sight range is how close a player must be for an enemy to see her.")] [SerializeField] [Range(0.0f, 100.0f)] float sightRange;
+    [Tooltip("The attack range is how close the player must be for the enemy to attack her. \nEnsure that this isn't too low- so the Enemy doesn't go INTO the player when attacking.")] [SerializeField][Range(0.0f, 100.0f)] float attackRange;
+    [Tooltip("The range of how far this enemy will patrol when no player is in range. \nEnsure this value isn't too high- So the enemy doesn't wander away.")] [SerializeField] [Range(2,100)] float patrolRange;
 
-    public void TakeDamage(int amount)
+
+
+    [Header("Important References")]
+    [Tooltip("Ensure that these are assigned at all times.")] [SerializeField] LayerMask GroundLayer;
+    [Tooltip("Ensure that these are assigned at all times.")] [SerializeField] LayerMask PlayerLayer;
+
+    [SerializeField] NavMeshAgent agent;
+
+    private Transform player;
+
+    private Vector3 walkPoint; // Patrolling destination
+    private bool walkPointSet; // (Is there a patrol destination currently set)
+
+    private bool hasAttacked, pInAttackRange, pInSightRange;
+
+    private void Awake()
     {
-        EnemyHealth -= amount;
+        player = FindObjectOfType<PlayerManager>().transform;
+        agent = GetComponent<NavMeshAgent>();
     }
+
 
     private void FixedUpdate()
     {
+        // This checks to see if the player is in sight and attack range.
+        pInSightRange = Physics.CheckSphere(transform.position, sightRange, PlayerLayer); // She is in the general range of sight 
+        pInAttackRange = Physics.CheckSphere(transform.position, attackRange, PlayerLayer); // She is in general range of being atkd  
+
+        if (!pInSightRange && !pInAttackRange) Patrolling();
+        if (pInSightRange && !pInAttackRange) ChasePlayer();
+        if (pInSightRange && pInAttackRange) AttackPlayer();
+
+
+
+        // Looks at the player 
         Transform playerPos = FindObjectOfType<PlayerManager>().transform;
 
-        transform.LookAt(playerPos);
+    }
 
-        
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.CompareTag("Player")) other.GetComponent<Health>().Damage(attackDamage); // (Only hurt the player)
+    //}
+
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) SearchWalkPoint(); // Find a walkpoint if there's no patrolling destination
+
+        if (walkPointSet)  // If there is a walkpoint, just make the navmesh travel to it
+        {
+            agent.SetDestination(walkPoint);
+            transform.LookAt(walkPoint);
+        }
+
+        Vector3 distanceToWalPoint = transform.position - walkPoint;
+
+
+        // Arrived at walkpoint
+        if (distanceToWalPoint.magnitude < 1f) walkPointSet = false; // Reached destination, now repeat patrol
         
     }
 
-    private void Update()
+    private void SearchWalkPoint() // Find a random position within the patrolling range to patrol to, and ensure that it IS ON THE GROUND so the enemy doesn't walk off of the map 
     {
-        if (EnemyHealth <= 0)
+        float randomZ = Random.Range(-patrolRange, patrolRange); 
+        float randomX = Random.Range(-patrolRange, patrolRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, GroundLayer)) walkPointSet = true; // Latch the lockpoint variable
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position); // Simply follows the player
+        transform.LookAt(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position); // Stops the enemy from moving when it attacks in order to stop it from continuously running. This could be changed later to make different, faster types of enemies
+
+        transform.LookAt(player);
+
+
+        // This initiates a delay between her attacks, dependent on attackRate.
+        if (!hasAttacked) 
         {
-            Die();
+            hasAttacked = true;
+
+            Attack();
+
+            Invoke(nameof(ResetAttack), attackRate); 
         }
     }
 
-    private void Die()
+    private void ResetAttack()
     {
-        Destroy(this.gameObject);
+        hasAttacked = false; 
     }
 
+
+
+    private void Attack()   /* This is the attack function. Currently, it just hits the player, but later, it can be more complex-- (projectiles, self-destruction, etc..) */
+    {
+        player.GetComponent<Health>().Damage(attackDamage); // Basic Hit method that will attack the player, dealing attackDamage damage. 
+    }
 }
