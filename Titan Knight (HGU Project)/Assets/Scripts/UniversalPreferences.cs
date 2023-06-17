1062afd98209ff2d9aa74938e7fadd77d9c5d9a1
Developer Jake <Mark.Gaskins@hgs.hiddengeniusproject.org>
2023-06-16 22:44:59 -0400

    Fixing the commit conflict
    Fixing

---
+++ Titan Knight (HGU Project)/Assets/Scripts/UniversalPreferences.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalPreferences : MonoBehaviour
+{
+    /// <summary>
+    /// The universal master volume of the game.
+    /// </summary>
+    public static float _universalVolume = 1;
+
+    public static float _fxVolume = 1;
+
+
+}
