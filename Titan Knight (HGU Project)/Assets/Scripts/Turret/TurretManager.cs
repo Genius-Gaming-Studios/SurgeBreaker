22814d3d10e9a81635c1d14a97951168906ae8be
Developer Jake <Mark.Gaskins@hgs.hiddengeniusproject.org>
2023-06-29 22:57:55 -0400

    1.9.2a Laser Turrets
    - Added: Laser Turrets
    - Changed: Build menu layout

--- Titan Knight (HGU Project)/Assets/Scripts/Turret/TurretManager.cs
+++ Titan Knight (HGU Project)/Assets/Scripts/Turret/TurretManager.cs
@@ -5,17 +5,28 @@
 
 using UnityEngine;
 
+public enum TurretTypes
+{
+    Bullet,
+    Laser
+}
+
+
 public class TurretManager : MonoBehaviour
 {
-
-    [Tooltip("The range in which the turret can reach enemies from.")] [SerializeField] [Range(75, 9999)] public int turretCost = 150;
+    [Header("Settings")]
+    [Tooltip("(Important) The type of this turret.")] [SerializeField] public TurretTypes TurretType; // Very important, decides how the entire turret will behave.
 
     [Header("General")]
+    [Tooltip("The range in which the turret can reach enemies from.")] [SerializeField] [Range(75, 9999)] public int turretCost = 150;
+
     [Tooltip("The range in which the turret can reach enemies from.")] [SerializeField] [Range(3, 100)] public float range = 15f;
     [Tooltip("The tip of the turret that its bullets will fire from.")] [SerializeField] Transform firePoint;
 
     [Tooltip("The turn speed of the hinge. Doesn't affect how fast the turret fires.")] [SerializeField] [Range(0, 30)] float turnSpeed = 10f;
     [Tooltip("The tag of the enemy. Ensure that all enemies have this tag name assigned to them.")] [SerializeField] string enemyTag = "Enemy";
+    [Tooltip("The part that contains the fire animation.")] [SerializeField] Animation fireAnimation;
+
     public Turret turretSettings; 
 
     [Header("Bullet Preferences")]
@@ -29,7 +40,12 @@ public class TurretManager : MonoBehaviour
     [Tooltip("The sound that can be heard when the gun is fired.")] [SerializeField] AudioClip fireSound;
     [Tooltip("The muzzle flash for the gun.")] [SerializeField] GameObject muzzleFlash;
 
-    [Tooltip("The part that contains the fire animation.")] [SerializeField] Animation fireAnimation;
+
+    [Header("Laser Preferences")]
+    [SerializeField] LineRenderer laserRenderer;
+    [Tooltip("The speed in which the enemy will take damage at.")] [SerializeField] [Range(0, 15)] float damageRate = 1f;
+    [Tooltip("The amount of damage the enemy will take every damageRate.")] [SerializeField] [Range(3, 150)] int laserPower = 1;
+
 
     private float fireCountdown;
     private float mzwaitTime = 1f;
@@ -44,7 +60,7 @@ public class TurretManager : MonoBehaviour
 
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
         coreFXPlayer = FindObjectOfType<GameManager>().CoreFXPlayer;
-
+        
         if (turretSettings == null) Debug.LogWarningFormat("Please attatch the turret settings to a Turret in this scene in order to utilize the Selling features.");
 
         mzwaitTime = fireRate;
@@ -81,31 +97,42 @@ public class TurretManager : MonoBehaviour
     {
         if (target == null)
         {
+            if (TurretType == TurretTypes.Laser)
+            {
+                laserRenderer.enabled = false; // Hides the laser if there is no target in range of the turret.
+                laserRenderer.GetComponent<AudioSource>().enabled = false;
+            }
+
             return;
+        }
+        else if (TurretType == TurretTypes.Laser)
+        { // Re-enable graphic and audio
+            laserRenderer.GetComponent<AudioSource>().enabled = true;
+            laserRenderer.enabled = true;
         }
 
         if (FindObjectOfType<GameManager>().currentMode != GameMode.Combat) return; // Ensure that it's in combat mode fire
 
         LockOnTarget();
 
-
-        if ( fireCountdown <= 0f)
+        if (TurretType == TurretTypes.Bullet)
         {
-            Fire();
+            if (fireCountdown <= 0f)
+            {
 
-            // mzTimer += Time.deltaTime;
-            // if (mzTimer > mzwaitTime)
-            // {
-            //     // muzzleFlash.SetActive(false);
-            // }
+                Fire();
 
-            fireCountdown = 1f / fireRate;
+                fireCountdown = 1f / fireRate;
+            }
+
+            fireCountdown -= Time.deltaTime;
         }
+        else if (TurretType == TurretTypes.Laser)
+        {
+            LaserFire();
 
-        fireCountdown -= Time.deltaTime;
-
-        
-
+            fireCountdown -= Time.deltaTime;
+        }
     }
 
 
@@ -116,6 +143,20 @@ public class TurretManager : MonoBehaviour
         Vector3 rotation = Quaternion.Lerp(Hinge.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         Hinge.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+    }
+
+    private void LaserFire()
+    {
+        /// Locks the laser onto the target. 
+        laserRenderer.SetPosition(0, firePoint.position);
+        laserRenderer.SetPosition(1, target.position);
+
+        if (fireCountdown <= 0f)
+        {
+            target.gameObject.GetComponent<Health>().Damage(laserPower);
+
+            fireCountdown = 1f / damageRate;
+        }
     }
 
     private void Fire()
